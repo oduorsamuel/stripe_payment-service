99b9fb9de443a82b14dfa9a16f8b338cad1c01f6
oduor <samuel@techfactory.ltd>
2019-10-30 16:39:00 +0300

    initial commit for payment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'payment';
+export class AppComponent implements OnInit {
+  customStripeForm: FormGroup;
+  formProcess: boolean;
+  submitted: boolean;
+  message: string;
+
+  stripe = {
+    cardNumber: '',
+    expMonth: '',
+    expYear: '',
+    cvv: '',
+  };
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {
+   this.customStripeForm = this.fb.group({
+
+    });
+  }
+  ngOnInit(): void {
+  }
+pay(form: NgForm) {
+  this.formProcess = true;
+  ( window as any).Stripe.setPublishableKey('pk_test_U9HUhT2na07Jb0ZRMddUhbaY00xs1arrv2');
+  ( window as any).Stripe.card.createToken({
+    number: form.value.cardNumber,
+    exp_month: form.value.expMonth,
+    exp_year: form.value.expYear,
+    cvc: form.value.cvc
+  }, (status: number, response: any) => {
+    this.formProcess = false;
+    if (status === 200) {
+      this.toastr.success('Hello world!', 'Toastr fun!');
+      this.toastr.success(`Success! Card token ${response.card.id}.`);
+      this.message = `Success! Card token ${response.card.id}.`;
+    } else {
+      this.toastr.error(response.error.message);
+      this.message = response.error.message ;
+    }
+  });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
